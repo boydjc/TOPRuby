d5b12902c0719cbf51f18a5418106e60202afb51
Joshua Boyd <jaboydfamily@gmail.com>
2022-01-20 20:53:50 -0500

    fixed indentions

--- caesar.rb
+++ caesar.rb
@@ -1,16 +1,16 @@
 def caesar_cipher(plaintext, shift=0)
-	ciphertext_arr = []
+  ciphertext_arr = []
 	
-	# get the ascii code value for each letter
-	plaintext_arr.split("").each do |letter|
-		if letter != ' '
-			base = letter.ord < 91 ? 65 : 97
-			ciphertext_arr.push(((((letter.ord - base) + shift) % 26)+ base).chr)
-		elif
-			# keep spaces
-			ciphertext_arr.push(letter)
-		end
-	end
+  # get the ascii code value for each letter
+  plaintext_arr.split("").each do |letter|
+    if letter != ' '
+      base = letter.ord < 91 ? 65 : 97
+      ciphertext_arr.push(((((letter.ord - base) + shift) % 26)+ base).chr)
+    elif
+      # keep spaces
+      ciphertext_arr.push(letter)
+    end
+  end
 
-	return ciphertext_arr.join(',');
+  return ciphertext_arr.join(',');
 end
