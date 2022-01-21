93575695410fff1aecad1a62614f05ac1c26c7d8
Joshua Boyd <jaboydfamily@gmail.com>
2022-01-20 20:39:39 -0500

    finished cipher

--- caesar.rb
+++ caesar.rb
@@ -1,9 +1,16 @@
-puts "Hello Github"
-
 def caesar_cipher(plaintext, shift=0)
-	puts "Plaintext: #{plaintext}"
-	puts "Shift: #{shift}"
+	ciphertext_arr = []
+	
+	# get the ascii code value for each letter
+	plaintext_arr.split("").each do |letter|
+		if letter != ' '
+			base = letter.ord < 91 ? 65 : 97
+			ciphertext_arr.push(((((letter.ord - base) + shift) % 26)+ base).chr)
+		elif
+			# keep spaces
+			ciphertext_arr.push(letter)
+		end
+	end
+
+	return ciphertext_arr.join(',');
 end
-
-
-caesar_cipher("testing", 4)
